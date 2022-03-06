678120fd74f4d7fd863f35a74932b15cfb07820f
srportto <ceeh1100@gmail.com>
2022-03-05 22:23:55 -0300

    melhoria uso do mapstruct

--- app/src/main/java/br/com/hmv/models/mappers/FuncionarioMapper.java
+++ app/src/main/java/br/com/hmv/models/mappers/FuncionarioMapper.java
@@ -4,7 +4,11 @@ import br.com.hmv.dtos.request.FuncionarioInsertRequestDTO;
 import br.com.hmv.dtos.responses.FuncionarioDefaultResponseDTO;
 import br.com.hmv.dtos.responses.FuncionarioForListResponseDTO;
 import br.com.hmv.models.entities.Funcionario;
+import br.com.hmv.models.enums.GrupoFuncaoFuncionarioEnum;
+import br.com.hmv.models.enums.StatusFuncionarioEnum;
+import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(config = MapperConfig.class, componentModel = "spring")
@@ -14,6 +18,12 @@ public interface FuncionarioMapper {
     Funcionario deDtoParaFuncionario(FuncionarioInsertRequestDTO dto);
 
     FuncionarioDefaultResponseDTO deFuncionarioParaDto(Funcionario entity);
+
+    @BeforeMapping
+    default void preparaAntesDeMapearEntityParaListDto(Funcionario entity, @MappingTarget FuncionarioForListResponseDTO dto) {
+        dto.setStatusFuncionario(StatusFuncionarioEnum.obterStatusConvenio(entity.getCodigoStatusFuncionario()));
+        dto.setGrupoFuncaoFuncionario(GrupoFuncaoFuncionarioEnum.obterGrupoFuncaoFuncionario(entity.getCodigoGrupoFuncao()));
+    }
 
     FuncionarioForListResponseDTO deEntityParaRespresentacaoEmLista(Funcionario entity);
 }
